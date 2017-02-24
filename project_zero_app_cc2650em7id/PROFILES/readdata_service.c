cb2ca49259ed1f9719d7dfa967061c94d2c6599d
Tommy <goristommyucf@knights.ucf.edu>
2017-02-23 22:13:16 -0500

    Creating a service with new characteristics for experimentation

---
+++ project_zero_app_cc2650em7id/PROFILES/readdata_service.c
@@ -0,0 +1,8 @@
+/*
+ * readdata_service.c
+ *
+ *  Created on: Feb 23, 2017
+ *      Author: Gio
+ */
+
+
---
+++ project_zero_app_cc2650em7id/PROFILES/readdata_service.h
@@ -0,0 +1,8 @@
+/*
+ * readdata_service.h
+ *
+ *  Created on: Feb 23, 2017
+ *      Author: Gio
+ */
+
+
